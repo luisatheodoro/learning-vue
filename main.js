bbab79218269a0fba77feb630d74d2432e987d33
luisatheodoro <theodoro.luisa@gmail.com>
2019-03-01 14:42:30 +0000

    computed properties

--- main.js
+++ main.js
@@ -2,22 +2,24 @@ var app = new Vue({  //creates a new vue instance
   //inside we can store data and actions
   el: '#app', //here we are making reference to the app id, we are linking this instance to html div
   data: {  //The instance can also have a property for data
+    brand: 'Vue Mastery',
     product: "Socks",
-    image: './assets/vmSocks-green.png',
+    selectedVariant: 0,
     link: 'http://www.amazon.com',
-    inStock: false,
     onSale: false,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
     variants: [
       {
         variantId: 2234,
         variantColor: 'green',
-        variantImage: './assets/vmSocks-green.png'
+        variantImage: './assets/vmSocks-green.png',
+        variantQuantity: 20
       },
       {
         variantId: 2235,
         variantColor: 'blue',
-        variantImage: './assets/vmSocks-blue.png'
+        variantImage: './assets/vmSocks-blue.png',
+        variantQuantity: 0
       }
     ],
     sizes: ['3', '4', '5', '6', '7', '8', '9'],
@@ -32,8 +34,19 @@ var app = new Vue({  //creates a new vue instance
         this.cart -= 1
       }
     },
-    updateProduct: function (variantImage) {
-      this.image = variantImage
+    updateProduct: function (index) {
+      this.selectedVariant = index
     },
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity
+    }
   }
 });
