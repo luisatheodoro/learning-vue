691b1823aeaa8323da51c8b8e8b7b7e28d8087e8
luisatheodoro <theodoro.luisa@gmail.com>
2019-03-01 10:12:43 +0000

    binding link

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var app = new Vue({  //creates a new vue instance
   el: '#app', //here we are making reference to the app id, we are linking this instance to html div
   data: {  //The instance can also have a property for data
     product: "Socks",
-    image: './assets/vmSocks-green.png'
+    image: './assets/vmSocks-green.png',
+    link: 'http://www.amazon.com'
   }
 });
